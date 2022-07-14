641c66466b2429d0639cac8805db892b5254ec02
pauline <e2194480@cmaisonneuve.qc.ca>
2022-07-14 09:27:16 -0400

    modification script

--- script/carrousel.js
+++ script/carrousel.js
@@ -9,7 +9,6 @@
     let elmCarrousel__fermeture = document.querySelector('.carrousel__fermeture');
     let elmCarrousel__radio = document.querySelector('.carrousel__radio');
     //console.log(elmGalerieImg[0].getAttribute('src'))
-    let a = 0;
     let index = 0;
     let ancien_index = -1;
     let compteur = 0;
